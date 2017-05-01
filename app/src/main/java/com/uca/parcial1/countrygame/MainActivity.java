87da2624bfd2401709e482928dc8e74203cc5ecd
deviantfero <fmorataya.04@gmail.com>
2017-05-01 17:45:00 -0600

    working adapter and list, test countries in MainActivity

--- app/src/main/java/com/uca/parcial1/countrygame/MainActivity.java
+++ app/src/main/java/com/uca/parcial1/countrygame/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ListView;
 import com.uca.parcial1.countrygame.data.Country;
 import com.uca.parcial1.countrygame.data.CountryAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +23,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.countries = new ArrayList<Country>();
+
         this.countries.add(new Country("El Salvador", "Bad Country", "spanish", 123));
+        this.countries.add(new Country("USA", "Bad Country", "spanish", 123));
+        this.countries.add(new Country("Testo", "Bad Country", "spanish", 123));
+
         this.countryList = (ListView) this.findViewById(R.id.countryList);
         adapter = new CountryAdapter(this, R.layout.list_element, countries);
         this.countryList.setAdapter(adapter);
