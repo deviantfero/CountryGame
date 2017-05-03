14f7bb7a25fb713db4c9fb96c66cd9b71b9f02b9
deviantfero <fmorataya.04@gmail.com>
2017-05-03 15:38:47 -0600

    add new EntryPoint for application, can select between play or learn

--- app/src/main/java/com/uca/parcial1/countrygame/MainActivity.java
+++ app/src/main/java/com/uca/parcial1/countrygame/MainActivity.java
@@ -1,44 +1,25 @@
 package com.uca.parcial1.countrygame;
 
-import android.support.v4.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-    implements CountryFragment.onCountrySelectedListener {
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(this.findViewById(R.id.mainFragmentContainer) != null) {
-            if(savedInstanceState != null)
-                return;
-
-            CountryFragment countryListFragment = new CountryFragment();
-            this.getSupportFragmentManager().beginTransaction()
-                    .add(R.id.mainFragmentContainer, countryListFragment).commit();
-        }
     }
 
-    public void onCountrySelected(int position) {
-        DetailsFragment countryDetail = (DetailsFragment)this.getSupportFragmentManager()
-                .findFragmentById(R.id.detailFragment);
+    public void setGame(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
 
-        if(countryDetail != null) {
-            countryDetail.setFocus(position);
-        } else {
-            DetailsFragment newDetail = new DetailsFragment();
-            Bundle args = new Bundle();
-            args.putInt(DetailsFragment.POSITION, position);
-            newDetail.setArguments(args);
-            FragmentTransaction transaction = this.getSupportFragmentManager().beginTransaction();
-
-            transaction.replace(R.id.mainFragmentContainer, newDetail);
-            transaction.addToBackStack(null);
-
-            transaction.commit();
-        }
+    public void setLearning(View v) {
+        Intent intent = new Intent(this, LearnActivty.class);
+        startActivity(intent);
     }
 }
