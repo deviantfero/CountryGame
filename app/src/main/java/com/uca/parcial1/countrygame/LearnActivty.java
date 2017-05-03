c37deed87a949f12c6fc533ff583ee0eb9fb4598
deviantfero <fmorataya.04@gmail.com>
2017-05-03 15:36:50 -0600

    move LearnActivity to be it's own in order to make way to the new MainActivity

---
+++ app/src/main/java/com/uca/parcial1/countrygame/LearnActivty.java
@@ -0,0 +1,44 @@
+package com.uca.parcial1.countrygame;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LearnActivty extends AppCompatActivity
+    implements CountryFragment.onCountrySelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+        if(this.findViewById(R.id.learnFragmentContainer) != null) {
+            if(savedInstanceState != null)
+                return;
+
+            CountryFragment countryListFragment = new CountryFragment();
+            this.getSupportFragmentManager().beginTransaction()
+                    .add(R.id.learnFragmentContainer, countryListFragment).commit();
+        }
+    }
+
+    public void onCountrySelected(int position) {
+        DetailsFragment countryDetail = (DetailsFragment)this.getSupportFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+
+        if(countryDetail != null) {
+            countryDetail.setFocus(position);
+        } else {
+            DetailsFragment newDetail = new DetailsFragment();
+            Bundle args = new Bundle();
+            args.putInt(DetailsFragment.POSITION, position);
+            newDetail.setArguments(args);
+            FragmentTransaction transaction = this.getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.learnFragmentContainer, newDetail);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
+        }
+    }
+}
